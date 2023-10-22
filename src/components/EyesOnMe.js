66d42cfb8d89c6b5c30a0015a1e718d647008274
paula <paulalaorga@gmail.com>
2023-10-22 19:31:04 +1100

    all

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React, { useState } from "react";
+
+function EyesOnMe() {
+
+    const [isFocus, setIsFocus] = useState(false);
+
+  const handleFocus = (e) => {
+    setIsFocus(true);
+
+    console.log('Good!');
+  };
+
+  const handleBlur = (e) => {
+    setIsFocus(false);
+
+    console.log('Hey! Eyes on me!');
+  };
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}   
+            >Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
